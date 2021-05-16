0573c60798e916a03ca8258e3a344650bd965d3b
mornir <jeromepott@hotmail.com>
2021-05-16 20:27:56 +0200

    Add sizing to photos

--- schemas/photo.js
+++ schemas/photo.js
@@ -29,15 +29,27 @@ export default {
       },
     },
     {
-      name: 'orientation',
-      title: 'Orientation',
+      name: 'sizeH',
+      title: 'Horizontal Size',
       type: 'string',
       options: {
         list: [
-          { title: 'Vertical', value: 'vertical' },
-          { title: 'Horizontal', value: 'horizontal' },
-        ], // <-- predefined values
-        layout: 'radio', // <-- defaults to 'dropdown'
+          { title: 'small', value: '1' },
+          { title: 'medium', value: '2' },
+          { title: 'big', value: '3' },
+        ],
+      },
+    },
+    {
+      name: 'sizeV',
+      title: 'Vertical Size',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'small', value: '1' },
+          { title: 'medium', value: '2' },
+          { title: 'big', value: '3' },
+        ],
       },
     },
   ],
